8b7de52e67786468b55f492428e763eaceadc1a7
Greg Palmer <gpalmer323@gmail.com>
2014-02-10 18:23:43 -0800

    Main view displays current date and header; grabs happy items from plist

--- Happy-App/KSTViewController.m
+++ Happy-App/KSTViewController.m
@@ -10,6 +10,9 @@
 
 @interface KSTViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *date;
+@property NSMutableArray *happyItems;
+
 @end
 
 @implementation KSTViewController
@@ -18,12 +21,33 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self getAndShowDate];
+    [self loadHappyItems];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)getAndShowDate
+{
+    NSDate *today = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    NSTimeZone *systemTimeZone = [NSTimeZone systemTimeZone];
+    [dateFormatter setTimeZone:systemTimeZone];
+    [dateFormatter setDateFormat:@"EEEE, MMMM d"];
+    NSString *dateString = [dateFormatter stringFromDate:today];
+    [_date setText:dateString];
+}
+
+-(void)loadHappyItems
+{
+    NSString *happyItemsPlistPath = [[NSBundle mainBundle] pathForResource:@"HappyItems" ofType:@"plist"];
+    NSArray *happyItemsArray = [NSArray arrayWithContentsOfFile:happyItemsPlistPath];
+    [self.happyItems addObjectsFromArray:happyItemsArray];
+    NSLog(@"Happy items: %@", happyItemsArray);
 }
 
 @end
